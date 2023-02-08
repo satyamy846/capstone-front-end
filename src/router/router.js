f1a51217b3c9fe9683cb892e4a181bfa97236bf8
satyamy846 <satyadav941@gmail.com>
2023-02-09 02:24:56 +0530

    changed

--- src/router/router.js
+++ src/router/router.js
@@ -65,7 +65,7 @@ const router = createRouter({
         {
           path: 'signup',
           name: 'teachersignup',
-          component: () => import('../views/signup/Teachersignup.vue'),
+          component: () => import('../views/signup/TeacherSignup.vue'),
           meta:{auth:false}
         },
         {
